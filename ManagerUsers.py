c9d0254ab053c6ecc09678ff2ed2766da531c753
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2021-06-28 18:51:52 -0300

    Python

---
+++ ManagerUsers.py
@@ -0,0 +1,5 @@
+chave=input("Digite o login: ").upper()
+nome=input("Digite o nome: ").upper()
+data=input("Digite a última data de acesso: ")
+estacao=input("Qual a última estação acessada: ").upper()
+usuarios[chave]=[nome, data, estacao]
